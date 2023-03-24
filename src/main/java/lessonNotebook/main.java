c71e4b0dc29a4bf525ff810739a3a2e390fc4168
kreper <kreper375@gmail.com>
2023-03-25 00:27:38 +0300

    lesson 5

---
+++ src/main/java/lessonNotebook/main.java
@@ -0,0 +1,42 @@
+package lessonNotebook;
+import lesson.notebooks;
+
+import java.util.List;
+import java.util.Scanner;
+public class main {
+    public static void main(String[] args) {
+        lesson.notebooks HonorMagicBook = new lesson.notebooks("Honor", "MagicBook", "Grey", 15.6, "LCD", 8, 256, "SSD");
+        lesson.notebooks XiaomiRedmiBook = new lesson.notebooks("Xiaomi", "RedmiBook", "Black", 15.6, "LCD", 8, 512, "SSD");
+        lesson.notebooks HP250G9 = new lesson.notebooks("HP", "250 G9", "Green", 17.0, "TFT", 16, 512, "SSD");
+        lesson.notebooks AcerExtensa = new lesson.notebooks("Acer", "Extensa", "Red", 15.6, "OLED", 32, 2048, "HDD 2.0");
+        lesson.notebooks AsusVivoBook = new lesson.notebooks("ASUS", "VivoBook", "White", 15.6, "TFT", 16, 1024, "SSD");
+
+        List<lesson.notebooks> notebookList = List.of(HonorMagicBook, XiaomiRedmiBook, HP250G9, AcerExtensa, AsusVivoBook);
+
+        Scanner scannerUser = new Scanner(System.in);
+        System.out.println("Выберите параметр для поиска, где:\n1 - по цвету\n2 - по бренду");
+        int choiceUserSearch = scannerUser.nextInt();
+        Scanner scannerRequestUserForSearch = new Scanner(System.in);
+        System.out.println("Напишите интересующий цвет или бренд: ");
+        String requestUserForSearch = scannerRequestUserForSearch.nextLine();
+
+        if (choiceUserSearch == 1) {
+            for (lesson.notebooks color : notebookList) {
+                if (color.getColor().equalsIgnoreCase(requestUserForSearch)) {
+                    System.out.println(color.printInfo());
+                }
+            }
+
+        } else if (choiceUserSearch == 2) {
+            for (notebooks brand : notebookList) {
+                if (brand.getBrand().equalsIgnoreCase(requestUserForSearch)) {
+                    System.out.println(brand.printInfo());
+                }
+            }
+        } else {
+            System.out.println("Ошибка.\nВыберите параметр для поиска, где:\n1 - по цвету\n2 - по бренду");
+        }
+
+        scannerUser.close();
+    }
+}
