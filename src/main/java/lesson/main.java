75a37de731bcd200a807f27fd61a231c4c72cc5e
kreper <kreper375@gmail.com>
2023-03-25 00:22:48 +0300

    lesson 5

--- src/main/java/lesson/main.java
+++ src/main/java/lesson/main.java
@@ -3,13 +3,13 @@ import java.util.List;
 import java.util.Scanner;
 public class main {
     public static void main(String[] args) {
-        notebooks ChuwiAH401 = new notebooks("Honor", "MagicBook", "Grey", 15.6, "LCD", 8, 256, "SSD");
-        notebooks SamsungS101 = new notebooks("Xiaomi", "RedmiBook", "Black", 15.6, "LCD", 8, 512, "SSD");
-        notebooks SamsungS150 = new notebooks("HP", "250 G9", "Green", 17.0, "TFT", 16, 512, "SSD");
-        notebooks AsusTUF504 = new notebooks("Acer", "Extensa", "Red", 15.6, "OLED", 32, 2048, "HDD 2.0");
-        notebooks AsusTUF555 = new notebooks("ASUS", "VivoBook", "White", 15.6, "TFT", 16, 1024, "SSD");
+        notebooks HonorMagicBook = new notebooks("Honor", "MagicBook", "Grey", 15.6, "LCD", 8, 256, "SSD");
+        notebooks XiaomiRedmiBook = new notebooks("Xiaomi", "RedmiBook", "Black", 15.6, "LCD", 8, 512, "SSD");
+        notebooks HP250G9 = new notebooks("HP", "250 G9", "Green", 17.0, "TFT", 16, 512, "SSD");
+        notebooks AcerExtensa = new notebooks("Acer", "Extensa", "Red", 15.6, "OLED", 32, 2048, "HDD 2.0");
+        notebooks AsusVivoBook = new notebooks("ASUS", "VivoBook", "White", 15.6, "TFT", 16, 1024, "SSD");
 
-        List<notebooks> notebookList = List.of(ChuwiAH401, SamsungS101, SamsungS150, AsusTUF504, AsusTUF555);
+        List<notebooks> notebookList = List.of(HonorMagicBook, XiaomiRedmiBook, HP250G9, AcerExtensa, AsusVivoBook);
 
         Scanner scannerUser = new Scanner(System.in);
         System.out.println("Выберите параметр для поиска, где:\n1 - по цвету\n2 - по бренду");
